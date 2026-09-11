c37b6b510fdb9fc77698303d98a6e8a6b24a9d4c
galan06 <jmpiryvg@gmail.com>
2026-09-10 20:26:06 -0400

    Update archivo.py
    Agregue otra variable "estado_civil " y lo mostre junto con todo los datos

--- archivo.py
+++ archivo.py
@@ -1,7 +1,9 @@
 nombre = "Jose Miguel"
 apellido = "Galan Vasquez"
 edad = 26
+estado_civil = "Soltero"
 
 print("Nombre: " + nombre)
 print("Apellido: " + apellido)
 print("Edad: " + edad)
+print("Estado Civil: " + estado_civil)
